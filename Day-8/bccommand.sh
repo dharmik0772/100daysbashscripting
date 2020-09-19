b85ad9e5ec6b48fcfd524cb5902b093e209b8df9
Dharmik <dharmik.0772@gmail.com>
2020-09-19 19:33:46 +0530

    day-8

---
+++ Day-8/bccommand.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+num1=20
+num2=5
+
+echo "$num1+$num2" | bc
+echo "$num1-$num2" | bc
+echo "20.5*5" | bc
+echo "scale=20;20.5/5" | bc
+echo "20.5%5" | bc
+
+num=4
+
+echo "scale=2;sqrt($num)" | bc -l
+echo "scale=2;3^3" | bc -l
