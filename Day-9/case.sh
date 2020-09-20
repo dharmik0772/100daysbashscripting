b93b57d90fe97fdcbfbb265b251f6cbfbd49e005
Dharmik <dharmik.0772@gmail.com>
2020-09-20 22:05:12 +0530

    day-9

---
+++ Day-9/case.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+vehicle=$1
+
+case $vehicle in
+    "car" )
+	echo "Rent of $vehicale is 100  dollar" ;;
+    "vn" )
+	echo "Rent of $vehicale is 100  dollar" ;;
+    * )
+	echo "Unknown vehicle" ;;
+esac
